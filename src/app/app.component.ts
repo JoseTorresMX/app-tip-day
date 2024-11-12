0deb5be70de903a92daebf2812af6df3e151e9f1
JoseTorres <joseangel.tosa@outlook.com>
2024-11-11 23:38:19 -0700

    consumiendo servicio en el componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdviceServiceService } from './advice-service.service';//importamos el servicio
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  advice: string='';
+  
+  constructor(private adviceService: AdviceServiceService){}//inicializamos el constructor
+
+  ngOnInit(){
+    this.getAdvice();
+  }
+  getAdvice(){
+    this.adviceService.getAdvice().subscribe(response => {this.advice = response.slip.advice;},error=>{console.error('Error advice: ',error);
+      
+    }
+  ); //llamamos al método del servicio para obtener el consejo y lo almacenamos en la variable advice
+  }
+
   title = 'consejo-appv4';
 }
